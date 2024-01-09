eab5e1c10b2d097490639c6273b31b2fc2c79a50
panda0330 <superdev330@gmail.com>
2024-01-10 01:04:30 +0900

    add invoice page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
