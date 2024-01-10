3f6d06cbb123fc7961e9fe53aaa33a29e8dd18d2
panda0330 <superdev330@gmail.com>
2024-01-10 11:21:49 +0900

    add title metadate

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,11 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices | Acme Dashboard',
+};
 
 export default async function Page() {
   return (
